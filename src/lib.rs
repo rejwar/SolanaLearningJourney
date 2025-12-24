00777fc76e00f2d08bfcf6136518fc830a7dda93
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-24 08:15:13 +0600

    My code

--- src/lib.rs
+++ src/lib.rs
@@ -367,3 +367,123 @@
 //     };
 //     Ok(())
 // }
+
+// use solana_program::{
+//     account_info::{next_account_info, AccountInfo},
+//     entrypoint,
+//     entrypoint::ProgramResult,
+//     msg,
+//     pubkey::Pubkey,
+//     system_program,
+// };
+
+// entrypoint! {
+//     process_instruction
+// }
+
+// pub fn process_instruction(
+//     program_id: &Pubkey,
+//     accounts: &[AccountInfo],
+//     _instruction_data: &[u8],
+// ) -> ProgramResult {
+//     let accounts_iter = &mut accounts.iter();
+//     let target_account = next_account_info(account_info)?;
+//     let account_pubkey = target_account.key;
+//     let lamports = target_account.lamports();
+//     let sol_balance = lamports as f64 / 1_000_000_000;
+
+//     if target_account.owner == &system_program::ID {
+//         msg! { " Traget == system"}
+//     };
+
+//     Ok(())
+// }
+
+// use solana_program::{
+//     account_info::{next_account_info, AccountInfo},
+//     entrypoint,
+//     entrypoint::ProgramResult,
+//     msg,
+//     pubkey::Pubkey,
+//     system_program,
+// };
+
+// entrypoint! {
+//     process_instruction
+// }
+
+// pub fn process_instruction(
+//     program_id: &Pubkey,
+//     accounts: &[AccountInfo],
+//     _instruction_data: &[u8],
+// ) -> ProgramResult {
+//     let accounts_iter = &mut accounts.iter();
+//     let target_account = next_account_info(accounts_iter)?;
+//     let account_pubkey = target_account.key;
+//     let lamports = target_account.lamports();
+//     let sol_balance = lamports as f64 / 1_000_000_000;
+
+//     if target_account.owner == &system_program::ID {
+//         msg! {" target == system"}
+//     };
+//     Ok(())
+// }
+
+// use solana_program::{
+//     account_info::{next_account_info, AccountInfo},
+//     entrypoint,
+//     entrypoint::ProgramResult,
+//     msg,
+//     pubkey::Pubkey,
+//     system_program,
+// };
+
+// entrypoint! {
+//     process_instruction
+// }
+
+// pub fn process_instruction(
+//     program_id: &Pubkey,
+//     accounts: &[AccountInfo],
+//     _instruction_data: &[u8],
+// ) -> ProgramResult {
+//     let accounts_iter = &mut accounts.iter();
+//     let target_account = next_account_info(account_iter)?;
+//     let account_pubkey = target_account.key;
+//     let lamports = target_account.lamports;
+//     let sol_balance = lamports as f64 / 1_000_000_000;
+//     if target_account.owner == &system_program::ID {
+//         msg! {"Target == system"}
+//     };
+//     Ok(())
+// }
+
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    pubkey::Pubkey,
+    system_program,
+};
+
+entrypoint! {
+    process_instruction
+}
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let target_account = next_account_info(accounts_iter)?;
+    let account_pubkey = target_account.key;
+    let lamports = target_account.lamports();
+    let sol_balance = lamports as f64 / 1_000_000_000;
+
+    if target_account.owner == &system_program::ID {
+        msg! {" Target == system"}
+    };
+    Ok(())
+}
